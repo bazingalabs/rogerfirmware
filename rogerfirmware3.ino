621b63ecac947b0bb6709e888be0248c10aed216
Erik Kallen <info@erikkallen.nl>
2011-12-22 11:23:03 +0100

    Initial working library commit

---
+++ rogerfirmware3.ino
@@ -0,0 +1,46 @@
+#include <SPI.h>
+
+
+
+
+
+  
+  void portInit() {
+      SPI.begin();
+      SPI.setClockDivider(SPI_CLOCK_DIV8);
+      SPI.setDataMode(SPI_MODE0);
+  }
+  
+ void setup() {
+   Serial.begin(57600);
+   // asm("cli");
+   delay(100);
+    portInit();
+    rfInit();
+    
+  //  if (MODE) {
+      FIFOReset();
+    //}
+    Serial.println("Booted");
+}
+byte buf[50];
+int i = 0;
+void loop() {
+  if (Serial.available()>0) {
+    ///Serial.println("AVAILABLE");
+    char c = Serial.read();
+    buf[i++] = c;
+    if (c == '\n') {
+        sendPacket(buf, i);
+        buf[i+1] = '\0';
+        Serial.print("Sent: ");
+        Serial.println((char *)buf);
+        i=0;
+    }
+  }
+  if (packetAvailable()) {
+    recvPacket();
+  }
+}
+
+
