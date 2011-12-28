bc9ccefe7ddf36eeb63abcc594784c411fd3da1f
Erik Kallen <info@erikkallen.nl>
2011-12-28 20:27:53 +0100

    Moved library part to its own repository

--- rogerfirmware.ino
+++ rogerfirmware.ino
@@ -1,9 +1,14 @@
 #include <SPI.h>
-#include "rf12b.h"
+#include <RF12B.h>
+#include <rf_packet.h>
+#include "MemoryFree.h"
 //RF12B * rf12b = RF12B::Instance();
-RF12B * rf12 = RF12B::Instance();
+//RF12B * rf12;
 void setup() {
+	
 	Serial.begin(57600);
+	RF12.begin();
+	//rf12 = RF12B::Instance();
    // asm("cli");
 	Serial.println("Booted");
 }
@@ -15,15 +20,19 @@ void loop() {
 		char c = Serial.read();
 		buf[i++] = c;
 		if (c == '\n') {
-			rf12->sendPacket(buf, i);
+			RF12.sendPacket(buf, i);
 			buf[i+1] = '\0';
 			Serial.print("Sent: ");
 			Serial.println((char *)buf);
 			i=0;
 		}
 	}
-	if (rf12->packetAvailable()) {
-		rf12->recvPacket();
+	if (RF12.packetAvailable()) {
+		RFPacket p = RF12.recvPacket();
+		p.dump();
+		//free(p);
+		Serial.print("freeMemory()=");
+		    Serial.println(freeMemory());
 	}
 }
 
